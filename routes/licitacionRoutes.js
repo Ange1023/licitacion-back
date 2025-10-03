2081a4740f27b2b6541316e4c64461876bcbddd6
ange1020 <angelina.30571079@uru.edu>
2025-10-03 01:34:54 -0400

    fixes

--- routes/licitacionRoutes.js
+++ routes/licitacionRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { seleccionarGanador } from '../controllers/licitacionController.js';
+import {procesarOferta, listarOfertas, subirOferta } from '../controllers/ofertasController.js';
+import upload from '../middleware/multerConfig.js';
 
 const router = express.Router();
 
-// Seleccionar Ganador
-
-  router.post('/licitacion/ganador', seleccionarGanador);
+  router.post('/ganador', seleccionarGanador);
+  router.get('/listar', listarOfertas);
+  router.post('/subir',upload.single('paquete_dat'), subirOferta);
+  router.post('/procesar/:id', procesarOferta);
 
 export default router;
